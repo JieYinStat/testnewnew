cb6842ded0f93a1f3aaac1f30ad0810015d70a71
JieYinStat <yjabcdhot@hotmail.com>
2024-02-12 10:46:13 +0800

    add function

---
+++ R/add.R
@@ -0,0 +1,15 @@
+#' Add Number
+#'
+#' add two numbers.
+#'
+#' @param a number1
+#' @param b number2
+#'
+#' @return a numer result
+#' @export
+#'
+#' @examples
+#' add(1,2)
+add <- function(a,b) {
+  a + b
+}
---
+++ tests/testthat/test-add.R
@@ -0,0 +1,3 @@
+test_that("equal a b", {
+  expect_equal(add(2,3), 5)
+})
